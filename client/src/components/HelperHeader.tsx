10aafc4486c7ec0bf23188d6b8c46c62db3e45b6
adityasayare88 <adityasayare2003@gmail.com>
2024-06-07 21:10:17 +0530

    code editor page's freontend done

--- client/src/components/HelperHeader.tsx
+++ client/src/components/HelperHeader.tsx
@@ -1,12 +1,47 @@
 import { Button } from "./ui/button";
+import { Save, Share2 } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function HelperHeader() {
   return (
     <div className="__helper_header h-[50px] bg-black text-white p-2 flex justify-between items-center">
-        <div className="__btn_container"></div>
-        <Button>Save</Button>
-        <Button>Share</Button>
-      HelperHeader
+      <div className="__btn_container flex gap-2.5">
+        <Button
+          className="flex justify-between items-center gap-2"
+          variant="success"
+        >
+          <Save size={16} />
+          Save
+        </Button>
+        <Button
+          className="flex justify-between items-center gap-2"
+          variant="secondary"
+        >
+          <Share2 size={16} />
+          Share
+        </Button>
+      </div>
+      <div className="__tab_switcher flex justify-center items-center gap-1">
+        <small>Current Language:</small>
+        <Select defaultValue="html">
+          <SelectTrigger className="w-[130px] bg-gray-700 outline-none focus:ring-0">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="html">HTML</SelectItem>
+            <SelectItem value="css">CSS</SelectItem>
+            <SelectItem value="javascript">Javascript</SelectItem>
+            <SelectItem value="python">Python</SelectItem>
+            <SelectItem value="java">Java</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
     </div>
   );
 }
