c99f25c1dc9c67f434239a332f38029083a33c0f
adityasayare88 <adityasayare2003@gmail.com>
2024-06-15 18:35:28 +0530

    making backend server and connecting it to Mongodb Atlas cluster0

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
+import { config } from "dotenv";
+import { dbConnect } from "./lib/dbConnect";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+config();
 
-app.get("/", (req: Request, res: Response) => {
-  return res.status(200).send("ok");
-});
-
+dbConnect();
 app.listen(4000, () => {
   console.log("http://localhost:4000");
 });
--- server/src/routes/compilerRouter.ts
+++ server/src/routes/compilerRouter.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express from "express";
+import { saveCode } from "../controllers/compilerController";
 
-export const compilerRouter = express.Router()
+export const compilerRouter = express.Router();
 
-compilerRouter.post()
+compilerRouter.post("/save", saveCode);
