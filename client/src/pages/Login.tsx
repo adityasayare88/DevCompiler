748f0a34ebcd685c2bcb887f39ad8b99d498ad8f
adityasayare88 <adityasayare2003@gmail.com>
2024-07-01 08:29:00 +0530

    signup and login page UI done on frontend using shadcn ui

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,79 @@
-import React from "react";
+//import React from "react";
 import "./pageStyles/grid.css";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router-dom";
+
+const formSchema = z.object({
+  userId: z.string(),
+  password: z.string(),
+});
 export const Login = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      userId: "",
+      password: "",
+    },
+  });
+  function handleLogin(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
-    <div className="___login grid-bg w-full h-[calc(100dvh-60px)] border-2">
-      Login
+    <div className="___login grid-bg w-full h-[calc(100dvh-60px)] flex justify-center items-center flex-col gap-3 space-y-4">
+      <div className="__form_container bg-blur border-[1px] py-6 px-4 flex flex-col gap-5 w-[300px]">
+        <div className="">
+          <h1 className="text-4xl font-bold text-left">Login here</h1>
+          <p className="font-mono">Welcome back fellow coders 🧑‍💻</p>
+        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleLogin)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              control={form.control}
+              name="userId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Username or Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full" type="submit">
+              Login
+            </Button>
+          </form>
+        </Form>
+        <small className="text-xs font-mono">
+          Already have an account ? <Link className="text-blue-500" to="/login">Login</Link>.
+        </small>
+      </div>
     </div>
   );
-};  
+};
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,10 +1,95 @@
-import React from 'react';
+//import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import "./pageStyles/Signup.css";
+import { Link } from "react-router-dom";
 
-export const Signup = () => {
+const formSchema = z.object({
+  userId: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  username: z.string(),
+});
+
+export default function Signup() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+  function handleSignup(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
-    <div className="signup-container">
-      Signup
+    <div className="___signup signup-container w-full h-[calc(100dvh-60px)] flex justify-center items-center flex-col gap-3 space-y-4">
+      <div className="__form_container bg-blur border-[1px] py-8 px-8 flex flex-col gap-5 w-[300px]">
+        <div className="">
+          <h1 className="text-4xl font-bold text-left">Signup</h1>
+          <p className="font-mono">Join the Community of Coders.</p>
+        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSignup)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              control={form.control}
+              name="userId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="email" placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full" type="submit">
+              Signup
+            </Button>
+          </form>
+        </Form>
+        <small className="text-xs font-mono">
+          Already have an account ? <Link className="text-blue-500" to="/login">Login</Link>.
+        </small>
+      </div>
     </div>
   );
-};
+}
